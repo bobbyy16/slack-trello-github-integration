7c4b8e427b171a98de43a23866931d1f42519f2f
Abhishek k <bobbyyyyy.16@gmail.com>
2024-05-11 12:39:25 +0530

    update X on closed issue or pull request

--- controllers/github.controllers.js
+++ controllers/github.controllers.js
@@ -34,21 +34,33 @@ const handleGitHubEvent = async (req, res) => {
         eventDescription += `Issue Title: ${eventData.issue.title}\n`;
         eventDescription += `Issue URL: ${eventData.issue.html_url}\n`;
         await createTrelloCard(eventTitle, eventDescription, eventType);
-        await postToSlack(eventTitle + "\n" + eventDescription, eventType);
+        await postToSlack(
+          eventTitle + "\n" + eventDescription,
+          eventType,
+          eventData
+        );
         break;
       case "PULL_REQUEST":
         eventDescription += `Pull Request Number: ${eventData.pull_request.number}\n`;
         eventDescription += `Pull Request Title: ${eventData.pull_request.title}\n`;
         eventDescription += `Pull Request URL: ${eventData.pull_request.html_url}\n`;
         await createTrelloCard(eventTitle, eventDescription, eventType);
-        await postToSlack(eventTitle + "\n" + eventDescription, eventType);
+        await postToSlack(
+          eventTitle + "\n" + eventDescription,
+          eventType,
+          eventData
+        );
         break;
       case "PUSH":
         eventDescription += `Pusher Username: ${eventData.pusher.name}\n`;
         eventDescription += `Commits Count: ${eventData.commits.length}\n`;
         eventDescription += `Compare URL: ${eventData.compare}\n`;
         await createTrelloCard(eventTitle, eventDescription, eventType);
-        await postToSlack(eventTitle + "\n" + eventDescription, eventType);
+        await postToSlack(
+          eventTitle + "\n" + eventDescription,
+          eventType,
+          eventData
+        );
         break;
       default:
         console.log("Unknown event type:", eventType);
